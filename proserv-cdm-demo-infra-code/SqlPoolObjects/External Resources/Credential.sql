6ea4c56eeb909b7ae05fa62f37cb953f4521e0b7
Mijo Joseph <mijo.joseph@neudesic.com>
2021-06-07 18:06:42 +0530

    Create master key if not exists

--- proserv-cdm-demo-infra-code/SqlPoolObjects/External Resources/Credential.sql
+++ proserv-cdm-demo-infra-code/SqlPoolObjects/External Resources/Credential.sql
@@ -1,2 +1,5 @@
-CREATE MASTER KEY ENCRYPTION BY PASSWORD = '<masterKeyPassword>'
+IF NOT EXISTS (SELECT * FROM sys.symmetric_keys WHERE name LIKE '%DatabaseMasterKey%')
+BEGIN
+    CREATE MASTER KEY ENCRYPTION BY PASSWORD = '<masterKeyPassword>'
+END;
 GO
