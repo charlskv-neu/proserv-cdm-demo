0b23eed80e9677091ade1539bdc2e20e6d9b9d38
Charls K V <charls89kv@gmail.com>
2021-06-23 17:20:03 +0530

    changes done to include new storage account and folder structure

---
+++ data-engineer-persona/README.md
@@ -0,0 +1,55 @@
+# *ProServ Demo Handbook*
+
+
+## *Prerequisites* : 
+ - Azure Subscription with a Resource Group to deploy
+ - User must have owner access to the resource group
+ - [Download and install](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-windows?tabs=azure-cli) the current release of the Azure CLI to run the deployment script. After the installation is complete, you will need to close and reopen any active Windows Command Prompt or PowerShell windows to use the Azure CLI.
+
+
+## Collect data needed to run the scripts
+
+Before you get started, copy and paste below variables in a text editor for later use.:
+
+
+| Variable Name		       | Description	             					    | How to Get			      |
+|----------------------------- | -------------------------------------------------------------------|------------------------------------------
+|TenantID | Unique identifier (Tenant ID) of the Azure Active Directory instance | Azure portal -> Azure Active Directory -> Copy TenantId |
+|SubscriptionId | Subscription Id where the deployment has to be done | Azure Portal -> Subscriptions ->  Select the subscription Name -> Copy Subscription ID |
+|ResourceGroupName | Resource Group name where the deployment to be made | Azure Portal -> Copy Resource Group Name |
+|SynapseWorkspaceName |	Synapse workspace name | A globally unique name with at most 50 characters long, must contain only lower-case letters, digits and hyphens but can not start or end with '-', and must not contain the string '-ondemand' anywhere in the name.
+|SyanpseDefaultADLSName | Default ADLS Storage account name to be linked with Synapse. | A globally unique name with only lowercase letters and numbers. Name must be between 3 and 24 characters.
+|AmericasADLSName | ADLS Storage account name in Americas region to be used for Third Party data. | A globally unique name with only lowercase letters and numbers. Name must be between 3 and 24 characters.
+|ApacADLSName | ADLS Storage account name in APAC region to be used for Contoso data. | A globally unique name with only lowercase letters and numbers. Name must be between 3 and 24 characters.
+
+## Prepare starter kit and run the client-side setup scripts
+
+1. [Download the starter kit](https://github.com/charlskv-neu/proserv-cdm-demo/tree/development) , and extract its contents to the location of your choice.
+
+2. On your computer, enter **PowerShell** in the search box on the Windows taskbar. In the search list, right-click **Windows PowerShell**, and then select **Run as administrator**.
+
+
+3. Use the following command to navigate to the directory where the setup script is residing in the Powershell IDE. Replace path-to-starter-kit with the folder path of the extracted Setup-ProServ-Demo-Environment.ps1 file.
+
+	```powershell
+	cd <path-to-starter-kit>
+	dir -Path <path-to-starter-kit> -Recurse | Unblock-File
+	```
+
+4. Use the following command to run the setup kit. 
+
+	- Replace the TenantID, SubscriptionID, ResourceGroupName, SynapseWorkspaceName, SyanpseDefaultADLSName, AmericasADLSName and ApacADLSName placeholders using the data collected earlier.
+	
+	```powershell
+	.\Setup-ProServ-Demo-Environment.ps1 -TenantId <TenantID> -SubscriptionId <SubscriptionId> -ResourceGroupName <ResourceGroupName> -SynapseWorkspaceName <SynapseWorkspaceName> -SynapseDefaultADLSName <SyanpseDefaultADLSName> -AmericasADLSName <AmericasADLSName> -ApacADLSName <ApacADLSName>
+	```
+	
+	- Complete the Azure login when prompted
+	
+5. Once the deployment is complete, verify the synapse and storage account are created. Also, confirm if the Synapse artifacts are deployed too.
+
+## Share and Recieve data using Azure Data Share for demo
+
+- Refer this [tutorial](https://docs.microsoft.com/en-us/azure/data-share/share-your-data?tabs=azure-portal) to share and recieve data using Azure Data Share for demo
+
+***
---
+++ data-engineer-persona/infra/Scripts/AddContainersInStorageAccount.ps1
@@ -0,0 +1,231 @@
+#Parameters needed by the script .
+Param(
+    [Parameter(Mandatory=$True)]
+    [string] $ResourceGroupName,
+    [Parameter(Mandatory=$True)]
+    [string] $SynapseDefaultADLSName,
+    [Parameter(Mandatory=$True)]
+    [string] $AmericasADLSName,
+    [Parameter(Mandatory=$True)]
+    [string] $ApacADLSName,
+    [Parameter(Mandatory=$True)]
+    [string] $SynapseDefaultADLSFileSystemName,
+    [Parameter(Mandatory=$True)]
+    [string] $DataSourcePath
+)
+
+$storageAccount = az storage account show --resource-group $ResourceGroupName --name $SynapseDefaultADLSName --only-show-errors
+$storageAccount = az storage account show --resource-group $ResourceGroupName --name $AmericasADLSName --only-show-errors
+$storageAccount = az storage account show --resource-group $ResourceGroupName --name $ApacADLSName --only-show-errors
+
+Function Create-StorageContainer($containerName, $SynapseDefaultADLSName)
+{
+    $container = az storage container show --name $containerName --account-name $SynapseDefaultADLSName --only-show-errors
+    if($container)  
+    {  
+        Write-Host "$containerName - container already exists."
+    }  
+    else  
+    { 
+        az storage container create --name $containerName --account-name $SynapseDefaultADLSName --public-access off --only-show-errors --output none
+        if (!$?) {
+            throw "An error occurred while creating container."
+        }
+        else {
+            Write-Host "Created container $containerName ."
+        }        
+    }
+}
+Function Create-FolderInContainer($containerName, $SynapseDefaultADLSName, $folderName)
+{
+    $folder = az storage fs directory show --file-system $containerName --name $folderName --account-name $SynapseDefaultADLSName --only-show-errors
+    if($folder)  
+    {  
+        Write-Host "$folderName - folder already exists."
+    }  
+    else  
+    {              
+        az storage fs directory create --account-name $SynapseDefaultADLSName --file-system $containerName --name $folderName --only-show-errors --output none
+        if (!$?) {
+            throw "An error occurred while creating folder."
+        }
+        else {
+            Write-Host "Created folder $folderName ."
+        }        
+    }
+}
+Function Create-FileInFolder($containerName, $SynapseDefaultADLSName, $folderName, $fileName, $filePath)
+{
+    $finalPath = $folderName + $fileName
+    $file = az storage fs file show --file-system $containerName --path $finalPath --account-name $SynapseDefaultADLSName --only-show-errors
+    if($file)  
+    {  
+        Write-Host "$fileName - file already exists."
+    }  
+    else  
+    {              
+        az storage fs file upload --account-name $SynapseDefaultADLSName --file-system $containerName --path $finalPath --source $filePath --only-show-errors --output none
+        if (!$?) {
+            throw "An error occurred while creating file."
+        }
+        else {
+            Write-Host "Created file $fileName inside $folderName ."
+        }        
+    }
+}
+Function Upload-FilesFromFolder($containerName, $SynapseDefaultADLSName, $folderName, $folderPath)
+{
+    $filesToUpload = Get-ChildItem -Path $folderPath
+    for ($i=0; $i -lt $filesToUpload.Count; $i++) {
+        $fileName = $filesToUpload[$i].Name
+        $filePath = $folderPath +$fileName
+        Create-FileInFolder $containerName $SynapseDefaultADLSName $folderName $fileName $filePath
+    }
+}
+
+$containerName = "cdmproservdataintegration"
+Create-StorageContainer $containerName $SynapseDefaultADLSName
+
+$folderName = "RAW/"
+Create-FolderInContainer $containerName $SynapseDefaultADLSName $folderName
+
+$folderName = "RAW/Dynamics/"
+Create-FolderInContainer $containerName $SynapseDefaultADLSName $folderName
+
+$filePath = $DataSourcePath + "/data/Dynamics/"
+Upload-FilesFromFolder $containerName $SynapseDefaultADLSName $folderName $filePath
+
+$folderName = "RAW/SAP/"
+Create-FolderInContainer $containerName $SynapseDefaultADLSName $folderName
+
+$filePath = $DataSourcePath + "/data/SAP/"
+Upload-FilesFromFolder $containerName $SynapseDefaultADLSName $folderName $filePath
+
+$folderName = "CDM/"
+Create-FolderInContainer $containerName $SynapseDefaultADLSName $folderName
+
+$filePath = $DataSourcePath + "/cdm/SAP/"
+Upload-FilesFromFolder $containerName $SynapseDefaultADLSName $folderName $filePath
+
+$folderName = "Data/"
+Create-FolderInContainer $containerName $SynapseDefaultADLSName $folderName
+
+
+##Americas Container and Folders
+
+##sageworks container and its folders
+$containerName = "sageworks"
+Create-StorageContainer $containerName $AmericasADLSName
+
+$folderName = "sageworks-sap/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "sageworks-sap/general-ledger/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "sageworks-sap/general-ledger/config/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "sageworks-sap/general-ledger/config/cdm-model/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "sageworks-sap/general-ledger/output/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "sageworks-sap/general-ledger/output/cdm/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "sageworks-sap/general-ledger/source/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "sageworks-sap/general-ledger/staging/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "sageworks-sap/general-ledger/staging/dataverse/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+##thirdpartydata container and its folders
+$containerName = "thirdpartydata"
+Create-StorageContainer $containerName $AmericasADLSName
+
+$folderName = "taxidata/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/config/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/config/cdm-model/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$filePath = $DataSourcePath + "/cdm/TaxiData/"
+Upload-FilesFromFolder $containerName $AmericasADLSName $folderName $filePath
+
+$folderName = "taxidata/nyctaxidata/output/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/output/cdm/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/output/cdm/nyctaximediumoutput/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/output/cdm/nyctaxilargeoutput/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/source/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/source/nyc-green/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/source/nyc-yellow/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/staging/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+$folderName = "taxidata/nyctaxidata/staging/dataverse/"
+Create-FolderInContainer $containerName $AmericasADLSName $folderName
+
+##APAC Container and Folders
+
+##Contoso container and its folders
+$containerName = "contoso"
+Create-StorageContainer $containerName $ApacADLSName
+
+$folderName = "contoso-dynamics/"
+Create-FolderInContainer $containerName $ApacADLSName $folderName
+
+$folderName = "contoso-dynamics/general-ledger/"
+Create-FolderInContainer $containerName $ApacADLSName $folderName
+
+$folderName = "contoso-dynamics/general-ledger/config/"
+Create-FolderInContainer $containerName $ApacADLSName $folderName
+
+$folderName = "contoso-dynamics/general-ledger/config/cdm-model/"
+Create-FolderInContainer $containerName $ApacADLSName $folderName
+
+$filePath = $DataSourcePath + "/cdm/GeneralLedger/"
+Upload-FilesFromFolder $containerName $ApacADLSName $folderName $filePath
+
+$folderName = "contoso-dynamics/general-ledger/output/"
+Create-FolderInContainer $containerName $ApacADLSName $folderName
+
+$folderName = "contoso-dynamics/general-ledger/output/cdm/"
+Create-FolderInContainer $containerName $ApacADLSName $folderName
+
+$folderName = "contoso-dynamics/general-ledger/source/"
+Create-FolderInContainer $containerName $ApacADLSName $folderName
+
+$fileName = "General Journal.xlsx"
+$filePath = $DataSourcePath + "/data/GeneralLedger/" + $fileName
+Create-FileInFolder $containerName $ApacADLSName $folderName $fileName $filePath
+
+$folderName = "contoso-dynamics/general-ledger/staging/"
+Create-FolderInContainer $containerName $ApacADLSName $folderName
+
+$folderName = "contoso-dynamics/general-ledger/staging/dataverse/"
+Create-FolderInContainer $containerName $ApacADLSName $folderName
